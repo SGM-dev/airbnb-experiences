06530a56b11ee53a4c97dcecd23e42912ef56db7
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-09-25 15:25:52 +0530

    build Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import heroImage from "../assets/photo-grid.png";
+
+export function Hero() {
+  return (
+    <header className="hero">
+      <img src={heroImage} alt="photo-grid" className="hero--img" />
+      <h1 className="hero--title">Online Experiences</h1>
+      <p className="hero--text">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </header>
+  );
+}
