341b16d061080200bfbd0b1d966c7648909939a1
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-09-25 17:00:54 +0530

    add src directly inside Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import starImg from "../assets/star.png";
-import katieZafares from "../assets/katie-zaferes.png";
 
 export default function Card() {
   return (
     <div className="card">
-      <img className="card--img" src={katieZafares} />
+      <img className="card--img" src="src/assets/katie-zaferes.png" />
       <div className="card--stats">
         <img className="rating--img" src={starImg} />
         <span className="rating--score ">5.0</span>
